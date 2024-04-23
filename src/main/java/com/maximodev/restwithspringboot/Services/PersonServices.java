c2ba27d8182efb9b5507c784ed63a43a6693e76f
Felipe Maximo <felipecontato2001@hotmail.com>
2024-04-23 01:05:19 -0300

    finalizando os metodos HTTP POST PUT E DELETE

--- src/main/java/com/maximodev/restwithspringboot/Services/PersonServices.java
+++ src/main/java/com/maximodev/restwithspringboot/Services/PersonServices.java
@@ -1,5 +1,7 @@
 package com.maximodev.restwithspringboot.Services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,6 +15,16 @@ public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
+    public List<Person> findAll(){
+        logger.info("Finding all people!");
+
+        List<Person> persons = new ArrayList<>();
+        for(int i = 0; i < 8; i++){
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
     public Person findById(String id){
         logger.info("Finding one person!");
 
@@ -22,6 +34,30 @@ public class PersonServices {
         person.setLastName("OFelps");
         person.setAddress("Diadema - São Paulo - Brasil");
         person.setGender("Male");
+        return person;
+    }
+
+    public Person create(Person person) {
+        logger.info("Creating one person!");
+        return person;
+    }
+
+    public Person update(Person person) {
+        logger.info("Updating one person!");
+        return person;
+    }
+
+    public void delete(String id) {
+        logger.info("Deleting one person!");
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person name " + i);
+        person.setLastName("Last name " + i);
+        person.setAddress("Address " + i);
+        person.setGender("Gender" + i);
         return person;
     }
     
